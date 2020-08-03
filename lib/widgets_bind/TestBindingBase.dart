0ce610ff2b593137d1b5c01dc285c81050a422ea
yulun <yulun@weidian.com>
2020-08-03 18:14:32 +0800

    增加代码注释

--- lib/widgets_bind/TestBindingBase.dart
+++ lib/widgets_bind/TestBindingBase.dart
@@ -3,6 +3,12 @@ import 'dart:developer' as developer;
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
+/// 模仿系统的WidgetsFlutterBinding，更好的理解mixin机制以及初始化流程
+/// 疑问1 执行顺序是基类的mixin依赖顺序哪个类mixin的依赖顺序
+///   答 依赖的是实际执行的类，也就是TestWidgetsFlutterBinding的on顺序
+/// 疑问2 TestSemanticsBinding没有依赖其他的mixin，为什么其他的有依赖
+///   答 是的，例如系统类WidgetsBinding有那么多依赖，只是继承了它们的方法，方便监听，如render的handleTextScaleFactorChanged，handleMetricsChanged
+/// 其实每个子mixin只要on 基类就行， 实际的执行顺序 实际在执行的类也就是TestWidgetsFlutterBinding的mixin依赖顺讯决定的。
 class TestBindingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -42,17 +48,6 @@ class TestWidgetsFlutterBinding extends TestBindingBase
         TestSemanticsBinding,
         TestRendererBinding,
         TestWidgetsBinding {
-  /// Returns an instance of the [WidgetsBinding], creating and
-  /// initializing it if necessary. If one is created, it will be a
-  /// [WidgetsFlutterBinding]. If one was previously initialized, then
-  /// it will at least implement [WidgetsBinding].
-  ///
-  /// You only need to call this method if you need the binding to be
-  /// initialized before calling [runApp].
-  ///
-  /// In the `flutter_test` framework, [testWidgets] initializes the
-  /// binding instance to a [TestWidgetsFlutterBinding], not a
-  /// [WidgetsFlutterBinding].
   static TestWidgetsBinding ensureInitialized() {
     if (TestWidgetsBinding.instance == null) {
       print("WidgetsFlutterBinding constr");
@@ -62,6 +57,8 @@ class TestWidgetsFlutterBinding extends TestBindingBase
   }
 }
 
+/// TestWidgetsFlutterBinding最后一个是widgetsbinding
+/// 根据mixin规则，这里的initInstances先被执行
 mixin TestWidgetsBinding
     on
         TestBindingBase,
@@ -77,19 +74,22 @@ mixin TestWidgetsBinding
 
   @override
   void initInstances() {
-    super.initInstances();
+    /// step 1
+    super.initInstances();/// 执行TestSemanticsBinding的initInstances
+    /// step 14
     _instance = this;
   }
 }
 
 mixin TestSemanticsBinding on TestBindingBase {
-  /// The current [SemanticsBinding], if one has been created.
   static TestSemanticsBinding get instance => _instance;
   static TestSemanticsBinding _instance;
 
   @override
   void initInstances() {
-    super.initInstances();
+    /// step 3
+    super.initInstances();    /// 执行TestRendererBinding的initInstances
+    /// step 13
     _instance = this;
   }
 }
@@ -106,31 +106,34 @@ mixin TestRendererBinding
 
   @override
   void initInstances() {
-    super.initInstances();
+    /// step 2
+    super.initInstances();    /// 执行TestPaintingBinding的initInstances
+    /// step 12
     _instance = this;
   }
 }
 
-mixin TestGestureBinding on TestBindingBase {
-  /// The singleton instance of this object.
-  static TestGestureBinding get instance => _instance;
-  static TestGestureBinding _instance;
-
+mixin TestPaintingBinding on TestBindingBase, TestServicesBinding {
   @override
   void initInstances() {
-    super.initInstances();
+    /// step 4
+    super.initInstances();    /// 执行TestSchedulerBinding的initInstances
+    /// step 11
     _instance = this;
   }
+  static TestPaintingBinding get instance => _instance;
+  static TestPaintingBinding _instance;
 }
 
 mixin TestSchedulerBinding on TestBindingBase, TestServicesBinding {
-  /// The current [SchedulerBinding], if one has been created.
   static TestSchedulerBinding get instance => _instance;
   static TestSchedulerBinding _instance;
 
   @override
   void initInstances() {
-    super.initInstances();
+    /// step 5
+    super.initInstances();    /// 执行TestServicesBinding的initInstances
+    /// step 10
     _instance = this;
   }
 }
@@ -141,19 +144,23 @@ mixin TestServicesBinding on TestBindingBase {
 
   @override
   void initInstances() {
-    super.initInstances();
+    /// step 6
+    super.initInstances();    /// 执行TestGestureBinding的initInstances
+    /// step 9
     _instance = this;
   }
 }
 
-mixin TestPaintingBinding on TestBindingBase, TestServicesBinding {
+mixin TestGestureBinding on TestBindingBase {
+  static TestGestureBinding get instance => _instance;
+  static TestGestureBinding _instance;
+
   @override
   void initInstances() {
+    /// step 7
+    /// 最后被执行，但其实初始化函数是第一个走完的
     super.initInstances();
+    /// step 8
     _instance = this;
   }
-
-  /// The current [PaintingBinding], if one has been created.
-  static TestPaintingBinding get instance => _instance;
-  static TestPaintingBinding _instance;
 }
