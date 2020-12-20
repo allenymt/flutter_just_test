eb839eca4fa822a6300d053dd7e868630e3f7acd
yulun <yulun@weidian.com>
2020-12-20 23:39:32 +0800

    增加demo

--- lib/touchevent/hitbehavior_demo.dart
+++ lib/touchevent/hitbehavior_demo.dart
@@ -17,32 +17,59 @@ class HitBehaviorDemo extends StatelessWidget {
       // 默认情况下，Row的空白处不响应点击事件，有两个方法
       // 1. 设置behavior为HitTestBehavior.opaque或者translucent
       // 2. Container设置背景色,任意背景色都可以
-      body: GestureDetector(
-        behavior: HitTestBehavior.deferToChild,
-        child: Container(
-          height: 50,
-          // color: Colors.transparent,
-          decoration: BoxDecoration(color: Colors.transparent),
-          padding: EdgeInsets.only(left: 5, right: 5),
-          alignment: Alignment.center,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Text(
-                "left text",
-                style: TextStyle(fontSize: 20),
-              ),
-              Text(
-                "right text",
-                style: TextStyle(fontSize: 20),
-              ),
-            ],
-          ),
+      body: buildChild2(),
+    );
+  }
+
+  Widget buildChild1() {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      child: Container(
+        height: 200,
+        // color: Colors.transparent,
+        // decoration: BoxDecoration(color: Colors.transparent),
+        padding: EdgeInsets.only(left: 5, right: 5),
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text(
+              "left text",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "right text",
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
         ),
-        onTap: () {
-          print("click");
-        },
       ),
+      onTap: () {
+        print("click");
+      },
+    );
+  }
+
+  Widget buildChild2() {
+    return Stack(
+      children: <Widget>[
+        Listener(
+          child: ConstrainedBox(
+            constraints: BoxConstraints.tight(Size(300.0, 300.0)),
+            child: DecoratedBox(decoration: BoxDecoration(color: Colors.red)),
+          ),
+          onPointerDown: (event) => print("first child"),
+        ),
+        Listener(
+          child: ConstrainedBox(
+            constraints: BoxConstraints.tight(Size(200.0, 200.0)),
+            child: Center(child: Text("左上角200*200范围内-空白区域点击")),
+          ),
+          onPointerDown: (event) => print("second child"),
+          //放开此行注释后，单词点击 first ,second都会响应，HitTestBehavior.opaque是不行的
+          behavior: HitTestBehavior.translucent,
+        )
+      ],
     );
   }
 }
